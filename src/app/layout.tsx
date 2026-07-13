81a128cd6f6fd0fa4263d7f1586c7320589cedbc
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-07-13 16:43:42 +0530

    Project Intialized for Revora

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Revora",
+  description:
+    "A community-driven platform for showcasing projects, receiving structured peer reviews, and improving through actionable feedback",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10">
+        Welcome to Revora
+      </h1>
+      <p className="text-center mt-4 text-lg">
+        A community-driven platform for showcasing projects, receiving
+        structured peer reviews, and improving through actionable feedback.
+      </p>
+    </div>
+  );
+}
