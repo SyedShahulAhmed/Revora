4110ba1bac4c180902e2a85e168fdf3b0ad12da6
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-07-19 21:39:13 +0530

    feat : Mounted components into the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+import AboutSection from "@/components/landing/AboutSection";
+import CTASection from "@/components/landing/Cta/CTASection";
+import FAQSection from "@/components/landing/Faq/FAQSection";
+import FeatureSection from "@/components/landing/Features/FeatureSection";
+import Footer from "@/components/landing/Footer";
+
 import HeroSection from "@/components/landing/HeroSection";
+import HowItWorks from "@/components/landing/how-it-works/HowItWorks";
 import { GridBackground } from "@/components/shared/GridBackground";
 
 export default function Home() {
   return (
     <GridBackground>
-      <HeroSection />
+      <main className="pt-24">
+        <HeroSection />
+        <AboutSection />
+        <FeatureSection />
+        <HowItWorks />
+        <FAQSection />
+        <CTASection />
+        <Footer />
+      </main>
     </GridBackground>
   );
 }
