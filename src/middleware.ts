4611e2ace6a60e38583dba965dd1349c384e09bb
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-07-22 21:56:39 +0530

    feat : Added Middleware proxy

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ["/dashboard/:path*", "/submit-project", "/profile/:path*"],
+};
