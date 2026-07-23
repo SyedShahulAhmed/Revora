d53f6fd4c30b7f52012a2f7e6174852a1de87499
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-07-23 21:40:30 +0530

    feat : Added Toaster and Wrapped AuthROunder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Oxanium } from "next/font/google";
 import "../styles/globals.css";
-import Navbar from "@/components/landing/Navbar";
 import { cn } from "@/lib/utils";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { Toaster } from "sonner";
 
 const oxanium = Oxanium({
   subsets: ["latin"],
@@ -25,10 +26,29 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", oxanium.variable, "font-sans")}
     >
-      <body className="min-h-screen flex flex-col">
-        <Navbar />
-        {children}
-      </body>
+      <AuthProvider>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            classNames: {
+              toast:
+                "bg-black border border-cyan-500/30 text-white shadow-lg shadow-cyan-500/10",
+
+              success:
+                "bg-cyan-500 text-black border border-cyan-400 shadow-lg shadow-cyan-500/20",
+
+              error:
+                "bg-black text-white border border-red-500/50 shadow-lg shadow-red-500/10",
+
+              warning:
+                "bg-black text-yellow-400 border border-yellow-500/40 shadow-lg shadow-yellow-500/10",
+
+              info: "bg-black text-cyan-400 border border-cyan-500/40 shadow-lg shadow-cyan-500/10",
+            },
+          }}
+        />
+        <body className="min-h-screen flex flex-col">{children}</body>
+      </AuthProvider>
     </html>
   );
 }
