b294c21d0a16155d7cf80d7b0b3c50d8a3856aa5
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-07-23 21:39:55 +0530

    feat : Made the Navbar only to root to the Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import CTASection from "@/components/landing/Cta/CTASection";
 import FAQSection from "@/components/landing/Faq/FAQSection";
 import FeatureSection from "@/components/landing/Features/FeatureSection";
 import Footer from "@/components/landing/Footer";
-
 import HeroSection from "@/components/landing/HeroSection";
 import HowItWorks from "@/components/landing/how-it-works/HowItWorks";
+import Navbar from "@/components/landing/Navbar";
 import { GridBackground } from "@/components/shared/GridBackground";
 
 export default function Home() {
   return (
     <GridBackground>
+      <Navbar />
       <main className="pt-24">
         <HeroSection />
         <AboutSection />
