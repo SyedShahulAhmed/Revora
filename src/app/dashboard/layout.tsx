80bcf5d490b02aa15f3a93ffcabbd701143e3519
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-07-25 22:32:53 +0530

    feat : Integrated Navbar into the Dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,11 @@
+import DashboardNavbar from "@/components/dashboard/Navbar/DashboardNavbar";
 import { GridBackground } from "@/components/shared/GridBackground";
 import { ReactNode } from "react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <GridBackground>
+      <DashboardNavbar />
       <div className="flex flex-1 flex-col">
         <main className="flex-1 p-8">{children}</main>
       </div>
