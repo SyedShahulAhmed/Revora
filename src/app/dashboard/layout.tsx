45dbbdc7cd520dfabd8e555fd3c8098aad869bba
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-07-26 18:53:01 +0530

    feat : Fixed the dashboard layout issues

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,23 @@
-import DashboardNavbar from "@/components/dashboard/Navbar/DashboardNavbar";
-import { GridBackground } from "@/components/shared/GridBackground";
+"use client";
+
 import { ReactNode } from "react";
+import { useAuth } from "@/hooks/useAuth";
+import { GridBackground } from "@/components/shared/GridBackground";
+import DashboardNavbar from "@/components/dashboard/Navbar/DashboardNavbar";
+import Loader from "@/components/shared/Loader";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const { loading } = useAuth();
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <GridBackground>
-      <DashboardNavbar />
-      <div className="flex flex-1 flex-col">
-        <main className="flex-1 p-8">{children}</main>
+      <div className="min-h-screen">
+        <DashboardNavbar />
+        <main className="p-10 text-white">{children}</main>
       </div>
     </GridBackground>
   );
